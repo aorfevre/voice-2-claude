613abd8fdb94a6650c25939239ab3b0628159f91
alex <aorfevre@gmail.com>
2026-04-02 16:59:54 +0200

    feat: add Claude Code hook notification script

---
+++ server/hook-notify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Reads Claude Code hook JSON from stdin, forwards to Terminal Remote server
+INPUT=$(cat)
+curl -s -X POST http://localhost:3000/api/hook \
+  -H 'Content-Type: application/json' \
+  -H "X-Hook-Event: $1" \
+  -d "$INPUT" &
